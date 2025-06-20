9addbeb2fb42ef0bd15f603e2eb765d8d6cd46ee
RackGamesDev <yesterdiscord7@gmail.com>
2025-06-20 12:29:55 +0200

    Servicio de autenticacion

--- src/app/shared/components/pruebas/pruebas.component.ts
+++ src/app/shared/components/pruebas/pruebas.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ConexionIgdbService } from '../../services/conexion-igdb.service';
 import { Juego } from '../../../core/models/juego';
+import { AutenticacionService } from '../../services/autenticacion.service';
+import { Usuario } from '../../../core/models/usuario';
 
 @Component({
   selector: 'app-pruebas',
@@ -9,13 +11,21 @@ import { Juego } from '../../../core/models/juego';
   styleUrl: './pruebas.component.css'
 })
 export class PruebasComponent {
-  constructor(private conexionIgdbService:ConexionIgdbService) {}
+  constructor(private conexionIgdbService:ConexionIgdbService, private autenticacionService:AutenticacionService) {}
   ngOnInit() {
     this.prueba();
   }
   prueba(){
-    this.conexionIgdbService.buscarId(11452).then((result: Juego[]) => {
+    this.autenticacionService.crearUsuario("pruebas3@gmail.com", "prueba2").then(() => {console.log("YAYAYAYAYAY");});
+    /*this.autenticacionService.leerUsuarios().then((result: Usuario[]) => {
       result.forEach(e => {console.log(e.nombre)})
     });
+    this.autenticacionService.correoExiste("Sabrinad89@gmail.com").then((resultado:boolean) => {
+      if (resultado) {
+        console.log("SI");
+      } else {
+        console.log("NO");
+      }
+    })*/
   }
 }
