189806682ec542c407b7adc7807e8166a2464f39
RackGamesDev <yesterdiscord7@gmail.com>
2025-06-20 10:50:25 +0200

    Avances

---
+++ src/app/core/models/juego.ts
@@ -0,0 +1,8 @@
+export interface Juego {
+    id:number;
+    nombre:String;
+    fecha:number;
+    resumen:String;
+    url:String;
+    portada:String;
+}
--- src/app/shared/services/conexion-igdb.service.ts
+++ src/app/shared/services/conexion-igdb.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConexionIgdbService {
+  private readonly API_CLIENTID = environment.igdbClientId;
+  private readonly API_SECRET = environment.igdbClientSecret;
+  private readonly API_TOKEN = environment.igdbToken;
 
-  constructor() { }
+  
+
+  constructor(private httpClient:HttpClient) { }
 }
