dd95bd9467c06a9816b70c77e9591d02bf847d87
RackGamesDev <yesterdiscord7@gmail.com>
2025-06-20 11:40:28 +0200

    Servicio IGDB terminado

--- src/app/core/models/juego.ts
+++ src/app/core/models/juego.ts
@@ -1,8 +1,25 @@
-export interface Juego {
-    id:number;
-    nombre:String;
-    fecha:number;
-    resumen:String;
-    url:String;
-    portada:String;
+export class Juego {
+    public id: number;
+    public nombre: string;
+    public fecha: number;
+    public resumen: string;
+    public url: string;
+    public portada: string;
+
+    constructor(
+        id: number,
+        nombre: string,
+        fecha: number,
+        resumen: string,
+        url: string,
+        portada: string
+    ) {
+        this.id = id;
+        this.nombre = nombre;
+        this.fecha = fecha;
+        this.resumen = resumen;
+        this.url = url;
+        this.portada = portada;
+    }
+    
 }
--- src/app/shared/services/conexion-igdb.service.ts
+++ src/app/shared/services/conexion-igdb.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { Juego } from '../../core/models/juego';
+
+
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +11,39 @@ export class ConexionIgdbService {
   private readonly API_CLIENTID = environment.igdbClientId;
   private readonly API_SECRET = environment.igdbClientSecret;
   private readonly API_TOKEN = environment.igdbToken;
+  private readonly API_URL_JUEGOS = environment.igdbJuegosUrl;
 
-  
+  async buscarNombre(nombre: String): Promise<Juego[]> {
+    const body = 'fields id,cover.image_id,name,release_dates,summary,url; search "' + nombre + '";';
+    return this.buscar(body);
+  }
+  async buscarId(id: number): Promise<Juego[]> {
+    const body = 'fields id,cover.image_id,name,release_dates,summary,url; where id = ' + id + ';';
+    return this.buscar(body);
+  }
 
-  constructor(private httpClient:HttpClient) { }
+  async buscar(body: any): Promise<Juego[]> {
+    const respuesta = await fetch(this.API_URL_JUEGOS, {
+      method: 'POST',
+      headers: {
+        'Client-ID': this.API_CLIENTID,
+        'Authorization': "Bearer " + this.API_TOKEN,
+        'Accept': 'application/json',
+        'Content-Type': 'text/plain'
+      }, body
+    });
+    const juegos: Juego[] = [];
+    if (respuesta.ok) {
+      const cuerpo = await respuesta.json();
+      cuerpo.forEach((e: { id: number; name: string; release_dates: number[]; summary: string; url: string; cover: string; }) => {
+
+        juegos.push(new Juego(e.id, e.name, e.release_dates?.[0], e.summary, e.url, "https://images.igdb.com/igdb/image/upload/t_cover_big" + e.cover + ".jpg"));
+      });
+    } else {
+      console.log("ERROR");
+    }
+    return juegos;
+  }
+
+  constructor() { }
 }
